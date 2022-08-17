7ddbc33a705c70010035d01b7c107a08be6833ea
Ronald Portier <ronald@portier.eu>
2022-08-17 22:42:52 +0200

    [UPD] Task 2 to 7 of tutorial

--- todoapp/app.js
+++ todoapp/app.js
@@ -1,9 +1,67 @@
-import { __info__, Component, mount, xml } from './owl.js'
+import {
+    __info__,
+    Component,
+    mount,
+    xml,
+    useRef,
+    onMounted,
+    useState,
+} from './owl.js'
 console.log('hello owl', __info__.version)
 
-// Owl Components
-class Root extends Component {
-    static template = xml`<div>todo app</div>`
+// -------------------------------------------------------------------------
+// Task Component
+// -------------------------------------------------------------------------
+class Task extends Component {
+    static template = xml/* xml */ `
+    <div class="task" t-att-class="props.task.isCompleted ? 'done' : ''">
+      <input type="checkbox" t-att-checked="props.task.isCompleted"/>
+      <span><t t-esc="props.task.text"/></span>
+    </div>`
+    static props = ['task']
 }
 
-mount(Root, document.body)
+// -------------------------------------------------------------------------
+// Root Component
+// -------------------------------------------------------------------------
+class Root extends Component {
+    static template = xml/* xml */ `
+    <div class="todo-app">
+        <input placeholder="Enter a new task"  t-on-keyup="addTask" t-ref="add-input" />
+        <div class="task-list">
+            <t t-foreach="tasks" t-as="task" t-key="task.id">
+                <Task task="task" />
+            </t>
+        </div>
+    </div>`
+    static components = { Task }
+
+    nextId = 1
+    tasks = useState([])
+
+    addTask(ev) {
+        // 13 is keycode for ENTER
+        if (ev.keyCode === 13) {
+            const text = ev.target.value.trim()
+            ev.target.value = ''
+            if (text) {
+                const newTask = {
+                    id: this.nextId++,
+                    text: text,
+                    isCompleted: false,
+                }
+                this.tasks.push(newTask)
+            }
+        }
+    }
+
+    setup() {
+        const inputRef = useRef('add-input')
+        onMounted(() => inputRef.el.focus())
+    }
+}
+
+// -------------------------------------------------------------------------
+// Setup
+// -------------------------------------------------------------------------
+mount(Root, document.body, { dev: true })
